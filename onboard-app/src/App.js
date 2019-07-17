30a9729a1aacd9536218d4a18c67e5858184defa
anthonypiazza <piazza.anthony.n@gmail.com>
2019-07-17 17:25:47 -0500

    Add LoginForm, Formik HOC, create validation schema, POST request to endpoint

---
+++ onboard-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import LoginForm from './LoginForm';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
